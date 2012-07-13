eb751ae6a810dcbcb36924d0d9f5751fbfd20040
Brigham Brown <me@brighambrown.com>
2012-07-13 12:58:59 -0700

    Implement map update step with some tests.

--- src/icfp2012/mine.clj
+++ src/icfp2012/mine.clj
@@ -18,21 +18,22 @@
 
 (declare find-char)
 (declare move-robot)
+(declare map-update)
 (declare rep->actual)
 (declare set-char)
 
 (defrecord Mine [grid state score extant-lambdas dead-lambdas]
   AMine
   (move-left [mine]
-    (move-robot mine -1 0))
+    (map-update (move-robot mine -1 0)))
   (move-right [mine]
-    (move-robot mine 1 0))
+    (map-update (move-robot mine 1 0)))
   (move-up [mine]
-    (move-robot mine 0 1))
+    (map-update (move-robot mine 0 1)))
   (move-down [mine]
-    (move-robot mine 0 -1))
+    (map-update (move-robot mine 0 -1)))
   (wait-turn [mine]
-    mine)
+    (map-update mine))
   (abort [mine]
     mine)
   (done? [mine]
@@ -57,7 +58,8 @@
 (defn- set-chars [grid & coords-chars]
   (loop [grid grid
          [x y ch & rest-coords-chars] coords-chars]
-    (let [updated (assoc grid y (assoc (nth grid y) x ch))]
+    (let [[ax ay] (rep->actual grid x y)
+          updated (assoc grid ay (assoc (nth grid ay) ax ch))]
       (if rest-coords-chars
         (recur updated rest-coords-chars)
         updated))))
@@ -77,26 +79,24 @@
         (let [{:keys [grid state score extant-lambdas dead-lambdas]} mine
               [robot-x robot-y] (find-char grid \R)
               new-robot-x (+ dirx robot-x)
-              new-robot-y (+ diry robot-y)
-              [actual-x actual-y] (rep->actual grid robot-x robot-y)
-              [new-actual-x new-actual-y] (rep->actual grid new-robot-x new-robot-y)]
+              new-robot-y (+ diry robot-y)]
           (when (not (inside? mine new-robot-x new-robot-y))
             [grid state score extant-lambdas dead-lambdas])
           (case (object-at mine new-robot-x new-robot-y)
-            (\space \.) [(set-chars grid     actual-x     actual-y \space
-                                         new-actual-x new-actual-y \R)
+            (\space \.) [(set-chars grid     robot-x     robot-y \space
+                                         new-robot-x new-robot-y \R)
                          state
                          score
                          extant-lambdas
                          dead-lambdas]
-            \\ [(set-chars grid     actual-x     actual-y \space
-                                new-actual-x new-actual-y \R)
+            \\ [(set-chars grid     robot-x     robot-y \space
+                                new-robot-x new-robot-y \R)
                 state
                 (+ score 25)
                 (dec extant-lambdas)
-                (inc dead-lambdas)] ;; check for last lambda
-            \O [(set-chars grid     actual-x     actual-y \space
-                                new-actual-x new-actual-y \R)
+                (inc dead-lambdas)]
+            \O [(set-chars grid     robot-x     robot-y \space
+                                new-robot-x new-robot-y \R)
                 :winning
                 (+ (* 50 dead-lambdas) score)
                 extant-lambdas
@@ -105,13 +105,76 @@
                  (let [new-rock-x (+ robot-x dirx dirx)]
                    (if (not= \space (object-at mine new-rock-x robot-y))
                      [grid state score extant-lambdas dead-lambdas]
-                     [(set-chars grid     actual-x     actual-y \space
-                                      new-actual-x new-actual-y \R
-                                        new-rock-x     actual-y \*)
+                     [(set-chars grid     robot-x     robot-y \space
+                                      new-robot-x new-robot-y \R
+                                       new-rock-x    robot-y \*)
                       state score extant-lambdas dead-lambdas]))
                  [grid state score extant-lambdas dead-lambdas])
             [grid state score extant-lambdas dead-lambdas]))]
     (->Mine grid state (dec score) extant-lambdas dead-lambdas)))
+
+(defn- map-update [mine]
+  (let [{:keys [grid state score extant-lambdas dead-lambdas]} mine]
+    (loop [x 1
+           y 1
+           [new-grid new-state] [grid state]]
+      (if (not (inside? mine x y))
+        (if (> y (count grid))
+          (->Mine new-grid new-state score extant-lambdas dead-lambdas)
+          (recur 1 (inc y) [new-grid new-state]))
+        (recur
+         (inc x) y
+         (let [ch (object-at mine x y)]
+           (case ch
+             \* (cond
+                 (= \space (object-at mine x (dec y)))
+                 [(set-chars new-grid x      y  \space
+                                      x (dec y) \*)
+                  (if (and (inside? mine x (- y 2))
+                           (= \R (object-at mine x (- y 2))))
+                    :losing
+                    new-state)]
+
+                 (and (= \* (object-at mine x (dec y)))
+                      (= \space (object-at mine (inc x) y))
+                      (= \space (object-at mine (inc x) (dec y))))
+                 [(set-chars new-grid      x       y  \space
+                                      (inc x) (dec y) \*)
+                  (if (and (inside? mine (inc x) (- y 2))
+                           (= \R (object-at mine (inc x) (- y 2))))
+                    :losing
+                    new-state)]
+
+                 (and (= \* (object-at mine x (dec y)))
+                      (or (not= \space (object-at mine (inc x) y))
+                          (not= \space (object-at mine (inc x) (dec y))))
+                      (= \space (object-at mine (dec x) y))
+                      (= \space (object-at mine (dec x) (dec y))))
+                 [(set-chars new-grid      x       y  \space
+                                      (dec x) (dec y) \*)
+                  (if (and (inside? mine (dec x) (- y 2))
+                           (= \R (object-at mine (dec x) (- y 2))))
+                    :losing
+                    new-state)]
+
+                 (and (= \\ (object-at mine x (dec y)))
+                      (= \space (object-at mine (inc x) y))
+                      (= \space (object-at mine (inc x) (dec y))))
+                 [(set-chars new-grid      x       y  \space
+                                      (inc x) (dec y) \*)
+                  (if (and (inside? mine (inc x) (- y 2))
+                           (= \R (object-at mine (inc x) (- y 2))))
+                    :losing
+                    new-state)] 
+
+                 :else
+                 [new-grid new-state])
+             
+             \L (if (= 0 extant-lambdas)
+                  [(set-chars new-grid x y \O) new-state]
+                  [new-grid new-state])
+
+             [new-grid new-state])))))))
 
 (defn execute-move [mine ch]
   (let [ch (if (char? ch) ch (.charAt ch 0))]
@@ -132,12 +195,14 @@
           (let [next (.read r)]
             (if (= -1 next)
               (if (> (count row) 0)
-                [(conj grid row) (max max-length (count row))] 
-                [grid max-length])
+                [(conj grid row) (max max-length (count row)) lambdas] 
+                [grid max-length lambdas])
               (let [ch (char next)]
                 (case ch
                   \newline (recur (conj grid row) [] (max max-length (count row)) lambdas)
+                  
                   (\R \# \* \L \O \. \space) (recur grid (conj row ch) max-length lambdas)
+                  
                   \\ (recur grid (conj row ch) max-length (inc lambdas)))))))]
     (->Mine (vec (map #(let [deficit (- max-length (count %))]
                         (if (> deficit 0)
--- test/icfp2012/test/mine.clj
+++ test/icfp2012/test/mine.clj
@@ -5,36 +5,43 @@
 (deftest test-move-left
   (let [mine (mine-from-thing "data/contest3.map")
         left (move-left mine)]
-    (is \space (object-at left 4 8))
-    (is \R (object-at left 3 8)))
+    (is (= \space (object-at left 4 8)))
+    (is (= \R (object-at left 3 8))))
   (let [mine (mine-from-thing "data/contest1.map")
         moved (move-left mine)]
-    (is \space (object-at moved 5 5))
-    (is \R (object-at moved 4 5))
-    (is \* (object-at moved 3 5)))
-)
+    (is (= \space (object-at moved 5 5)))
+    (is (= \R (object-at moved 4 5)))
+    (is (= \* (object-at moved 3 4)))))
 
 (deftest test-move-right
   (let [mine (mine-from-thing "data/contest3.map")
         moved (move-right mine)]
-    (is \space (object-at moved 4 8))
-    (is \R (object-at moved 5 8)))
+    (is (= \space (object-at moved 4 8)))
+    (is (= \R (object-at moved 5 8))))
   (let [mine (mine-from-string "#####
 #R* #
 #####")
         moved (move-right mine)]
-    (is \space (object-at moved 2 2))
-    (is \R (object-at moved 3 2))
-    (is \* (object-at moved 4 2))))
+    (is (= \space (object-at moved 2 2)))
+    (is (= \R (object-at moved 3 2)))
+    (is (= \* (object-at moved 4 2)))))
 
 (deftest test-move-down
   (let [mine (mine-from-thing "data/contest1.map")
         moved (move-down mine)]
-    (is \space (object-at moved 5 5))
-    (is \R (object-at moved 5 4))))
+    (is (= \space (object-at moved 5 5)))
+    (is (= \R (object-at moved 5 4)))))
 
 (deftest test-move-up
   (let [mine (mine-from-thing "data/contest2.map")
         moved (move-up mine)]
-    (is \space (object-at moved 2 2))
-    (is \R (object-at moved 2 3))))
+    (is (= \space (object-at moved 2 2)))
+    (is (= \R (object-at moved 2 3)))))
+
+(deftest update-replaces-closed-list
+  (let [mine (mine-from-string "###
+#R#
+#L#")
+        moved (wait-turn mine)]
+    (is (= 0 (:extant-lambdas moved)))
+    (is (= \O (object-at moved 2 1)))))
